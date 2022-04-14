5db0338ed2d79c910056b3000b114f57f48953b3
tegsar <103316211+tegsar@users.noreply.github.com>
2022-04-14 08:29:25 +0700

    Update vpn.sh

--- ssh/vpn.sh
+++ ssh/vpn.sh
@@ -25,7 +25,7 @@ fi
 # Mod By SL
 # ==================================================
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/senowahyu62/scriptvps/main/ssh"
+akbarvpn="raw.githubusercontent.com/tegsar/colong/main/ssh"
 
 # initialisasi var
 export DEBIAN_FRONTEND=noninteractive
