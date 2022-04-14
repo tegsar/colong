d70a4f63dbf034515eb12cf43de56e3dccd32026
tegsar <103316211+tegsar@users.noreply.github.com>
2022-04-14 08:33:56 +0700

    Update sstp.sh

--- sstp/sstp.sh
+++ sstp/sstp.sh
@@ -23,7 +23,7 @@ echo -e "${NC}${LIGHT}Fuck You!!"
 exit 0
 fi
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/fisabiliyusri/Mantap/main/sstp"
+akbarvpn="raw.githubusercontent.com/tegsar/colong/main/sstp"
 
 MYIP=$(wget -qO- ipinfo.io/ip);
 MYIP2="s/xxxxxxxxx/$MYIP/g";
