37589fc728e75b9cc67b91d28b5fd0d787102239
tegsar <103316211+tegsar@users.noreply.github.com>
2022-04-14 08:33:11 +0700

    Update ssr.sh

--- ssr/ssr.sh
+++ ssr/ssr.sh
@@ -23,7 +23,7 @@ echo -e "${NC}${LIGHT}Fuck You!!"
 exit 0
 fi
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/fisabiliyusri/Mantap/main/ssr"
+akbarvpn="raw.githubusercontent.com/tegsar/colong/main/ssr"
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
