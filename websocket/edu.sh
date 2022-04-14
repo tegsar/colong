7b20bf70e4008166de23e1c78921298b0ac14178
tegsar <103316211+tegsar@users.noreply.github.com>
2022-04-14 08:35:02 +0700

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -4,7 +4,7 @@
 # ==========================================
 
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/fisabiliyusri/Mantap/main/websocket"
+akbarvpn="raw.githubusercontent.com/tegsar/colong/main/websocket"
 
 # Getting Proxy Template
 wget -q -O /usr/local/bin/ws-nontls https://${akbarvpn}/websocket.py
